5007d11396fdfd20a98c502a036ad8abf1a60f8b
= <=>
2024-04-02 08:39:12 +0400

    Exchange Currency Option is Added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      mode: 'AMD -> USD',
+      amdAmount: 0,
+      usdAmount: 0
+    }
+  },
+  methods: {
+    convertCurrency() {
+      if (this.mode === 'AMD -> USD') {
+        this.usdAmount = this.amdAmount / 400
+      } else {
+        this.amdAmount = this.usdAmount * 400
+      }
+    },
+    toggleMode() {
+      this.mode = this.mode === 'AMD -> USD' ? 'USD -> AMD' : 'AMD -> USD'
+      this.amdAmount = 0
+      this.usdAmount = 0
+    }
+  }
+}
 </script>
 
 <template>
@@ -13,35 +34,73 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <div class="exchange-container">
+      <h2>{{ mode }}</h2>
+      <div v-if="mode === 'AMD -> USD'" class="input-container">
+        <input type="number" v-model="amdAmount" @input="convertCurrency" class="input-field" />
+        <input
+          type="number"
+          :value="usdAmount"
+          disabled
+          class="input-field"
+          :class="{ disabled: true }"
+        />
+      </div>
+      <div v-else class="input-container">
+        <input type="number" v-model="usdAmount" @input="convertCurrency" class="input-field" />
+        <input
+          type="number"
+          :value="amdAmount"
+          disabled
+          class="input-field"
+          :class="{ disabled: true }"
+        />
+      </div>
+      <button @click="toggleMode" class="toggle-button">Change</button>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
+}
+.exchange-container {
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #f0f0f0;
+  border-radius: 5px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.input-container {
+  margin-bottom: 10px;
+}
+.input-field {
+  width: 50%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.input-field.disabled {
+  background-color: #f0f0f0;
+}
+
+.toggle-button {
+  padding: 10px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.toggle-button:hover {
+  background-color: #0056b3;
 }
 </style>
